16776ae0aed006e3119dffad21069c8517b5415b
Ashutosh Shukla <ashutosh6471@gmail.com>
2026-08-08 01:50:34 +0000

    feat(api): setup server entrypoint

---
+++ apps/api/src/server.ts
@@ -0,0 +1,18 @@
+import { createApp } from "./app.js";
+import { loadConfig } from "./config.js";
+
+const config = loadConfig();
+const app = await createApp({ config });
+
+const signals: NodeJS.Signals[] = ["SIGINT", "SIGTERM"];
+for (const signal of signals) {
+  process.on(signal, () => {
+    app.log.info({ signal }, "Shutting down API");
+    void app.close().finally(() => process.exit(0));
+  });
+}
+
+await app.listen({
+  host: config.API_HOST,
+  port: config.API_PORT
+});
